1f162b7fd6401519baf6c85ceaecbf430bbfa7bd
Ondřej Vondra <ondrej.vondra@live.com>
2018-06-29 00:28:01 +0200

    creating webhook on localhost server

--- src/server/main.go
+++ src/server/main.go
@@ -6,6 +6,9 @@ import (
   "fmt"
   httptransport "github.com/go-openapi/runtime/client"
   apiclient "github.com/mluvii/publicapi-go/client"
+  "github.com/go-openapi/strfmt"
+  "github.com/mluvii/publicapi-go/client/webhooks"
+  "github.com/mluvii/publicapi-go/models"
 )
 
 type EventPayload struct {
@@ -25,6 +28,22 @@ func processMluviiEvent(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+  transport := httptransport.New("127.0.0.1:44301", "", nil)
+  client := apiclient.New(transport, strfmt.Default)
+
+  callbackUrl := "http://localhost:5000/mluviiwebhook"
+
+  postParams := webhooks.NewAPIV1WebhooksPostParams();
+  postParams.Model = &models.PublicAPIWebhookModelsWebhookModel{
+    &callbackUrl,
+    []string{"SessionStarted", "SessionForwarded", "SessionEnded"},
+  }
+
+  _, err := client.Webhooks.APIV1WebhooksPost(postParams)
+  if _, ok := err.(*webhooks.APIV1WebhooksPostConflict); !ok {
+    panic(err)
+  }
+
   http.HandleFunc("/mluviiwebhook", processMluviiEvent)
   if err := http.ListenAndServe(":5000", nil); err != nil {
     panic(err)
