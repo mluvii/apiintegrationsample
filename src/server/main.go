b41697141a6dee987f627de34e7a11e3e1570447
Ondřej Vondra <ondrej.vondra@live.com>
2018-06-28 16:13:40 +0200

    simple mluvii webhook

---
+++ src/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "net/http"
+  "encoding/json"
+  "fmt"
+)
+
+type EventPayload struct {
+  EventType string
+  Data map[string]interface{}
+}
+
+func processMluviiEvent(w http.ResponseWriter, r *http.Request) {
+  decoder := json.NewDecoder(r.Body)
+  var ep EventPayload
+  err := decoder.Decode(&ep)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println(ep.EventType)
+  fmt.Println(ep.Data)
+}
+
+func main() {
+  http.HandleFunc("/mluviiwebhook", processMluviiEvent)
+  if err := http.ListenAndServe(":5000", nil); err != nil {
+    panic(err)
+  }
+}
