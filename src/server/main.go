5a780c4192ca69a2f323a15ea49a95c254fe929f
Ondřej Vondra <ondrej.vondra@live.com>
2018-06-29 13:28:50 +0200

    post or put webhook

--- src/server/main.go
+++ src/server/main.go
@@ -16,6 +16,34 @@ type EventPayload struct {
   Data map[string]interface{}
 }
 
+func postOrPutWebhook(client *apiclient.Mluviiapi, model models.PublicAPIWebhookModelsWebhookModel) (bool, error) {
+  postParams := webhooks.NewAPIV1WebhooksPostParams();
+  postParams.Model = &model
+
+  _, err := client.Webhooks.APIV1WebhooksPost(postParams)
+  if _, conflict := err.(*webhooks.APIV1WebhooksPostConflict); conflict {
+    putParams := webhooks.NewAPIV1WebhooksByIDPutParams()
+    putParams.ID = err.(*webhooks.APIV1WebhooksPostConflict).Payload
+    putParams.Model = postParams.Model
+    _, err := client.Webhooks.APIV1WebhooksByIDPut(putParams)
+    return err == nil, err
+  }
+
+  return err == nil, err
+}
+
+func subscribeToEvents(client *apiclient.Mluviiapi) {
+  callbackUrl := "http://localhost:5000/mluviiwebhook"
+  model := models.PublicAPIWebhookModelsWebhookModel{
+    &callbackUrl,
+    []string{"SessionStarted", "SessionForwarded", "SessionEnded", "UserStatusChanged"},
+  }
+
+  if ok, err := postOrPutWebhook(client, model); !ok {
+    panic(err)
+  }
+}
+
 func processMluviiEvent(w http.ResponseWriter, r *http.Request) {
   decoder := json.NewDecoder(r.Body)
   var ep EventPayload
@@ -31,18 +59,7 @@ func main() {
   transport := httptransport.New("127.0.0.1:44301", "", nil)
   client := apiclient.New(transport, strfmt.Default)
 
-  callbackUrl := "http://localhost:5000/mluviiwebhook"
-
-  postParams := webhooks.NewAPIV1WebhooksPostParams();
-  postParams.Model = &models.PublicAPIWebhookModelsWebhookModel{
-    &callbackUrl,
-    []string{"SessionStarted", "SessionForwarded", "SessionEnded"},
-  }
-
-  _, err := client.Webhooks.APIV1WebhooksPost(postParams)
-  if _, ok := err.(*webhooks.APIV1WebhooksPostConflict); !ok {
-    panic(err)
-  }
+  subscribeToEvents(client)
 
   http.HandleFunc("/mluviiwebhook", processMluviiEvent)
   if err := http.ListenAndServe(":5000", nil); err != nil {
